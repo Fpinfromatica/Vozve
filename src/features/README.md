7dcb84f314414f6930347a7e3a5587016137d6b9
VozVenezuela <fp.informatica2026@gmail.com>
2026-07-14 00:04:34 +0200

    chore: reorganize mobile/src and remove root src duplicates

--- src/features/README.md
+++
@@ -1,3 +0,0 @@
-# Features placeholder
-
-Create feature modules (auth, map, reports, salaries) inside this folder.
--- src/shared/components/Input.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { TextInput } from 'react-native';
-
-export default function Input(props: any) {
-  return <TextInput {...props} />;
-}
